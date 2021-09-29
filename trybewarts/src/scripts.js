090260f90dcec1ff219c13ac12fb0fc0b61c5d6c
Bruno Marques <blmarques.dev@gmail.com>
2021-09-29 09:05:32 -0300

    Finish TrybWarts

--- trybewarts/src/scripts.js
+++ trybewarts/src/scripts.js
@@ -24,6 +24,7 @@ contractCheck.addEventListener('click', () => {
     btnSend.disabled = false;
   } else {
     btnSend.disabled = true;
+    btnSend.preventDefault();
   }
 });
 
@@ -31,4 +32,96 @@ const contaCaracter = query('#textarea');
 contaCaracter.addEventListener('keyup', (event) => {
   const total = event.target.maxLength - event.target.textLength;
   query('.result').innerHTML = `Caracteris restantes: ${total}`;
-}, false);
+});
+
+// forms
+
+function getNome() {
+  const nome = query('#input-name').value; return nome;
+}
+
+function getsubNome() {
+  const subNome = query('#input-lastname').value; return subNome;
+}
+
+function getMail() {
+  const mail = query('#input-email').value; return mail;
+}
+
+function getCasa() {
+  const casa = query('#house').value; return casa;
+}
+
+function getObservacoes() {
+  const text = query('#textarea').value; return text;
+}
+
+function family() {
+  const familia = document.getElementsByName('family');
+  let fami = '';
+  familia.forEach((radio) => {
+    if (radio.checked) {
+      fami = radio.value;
+    }
+  });
+  return fami;
+}
+
+function rateRange() {
+  const avaliacao = document.getElementsByName('rate');
+  let rat = 0;
+  avaliacao.forEach((radio) => {
+    if (radio.checked) {
+      rat = radio.value;
+    }
+  });
+  return rat;
+}
+
+function getMaterias() {
+  const meterias = document.getElementsByName('aprender');
+  const lessions = [];
+  meterias.forEach((subject) => {
+    if (subject.checked) {
+      const val = subject.value;
+      lessions.push(val);
+    }
+  });
+  return lessions.join(', ');
+}
+
+function getData() {
+  const persona = {
+    name: getNome(),
+    subNome: getsubNome(),
+    email: getMail(),
+    casa: getCasa(),
+    familia: family(),
+    materias: getMaterias(),
+    avaliacao: rateRange(),
+    observacoes: getObservacoes(),
+  };
+  return persona;
+}
+
+function newFrame() {
+  const person = getData();
+  const newForm = query('#evaluation-form');
+  newForm.innerHTML = `<div class="data">
+  <table class=" table"> <tbody>
+  <tr class="row"><th> Nome: </th><td> ${person.name} ${person.subNome}</td></tr>
+  <tr class="row"><th> Email: </th><td> ${person.email} </td></tr>
+  <tr class="row"><th> Casa: </th><td> ${person.casa} </td></tr>
+  <tr class="row"><th> Família: </th><td> ${person.familia} </td></tr>
+  <tr class="row"><th> Matérias: </th><td> ${person.materias} </td></tr>
+  <tr class="row"><th> Avaliação: </th><td> ${person.avaliacao} </td></tr>
+  <tr class="row"><th> Observações: </th><td> ${person.observacoes} </td></tr>
+  </tbody></table> </div>
+  `;
+}
+
+const btnEnviar = query('#submit-btn');
+btnEnviar.addEventListener('click', (event) => {
+  event.preventDefault();
+  newFrame();
+});
